51c1baeda981b27b0dc6406feab5caa18f580da3
Lasmar Safouane <146588910+safone21@users.noreply.github.com>
2025-04-27 15:15:54 +0100

    Create quality_checks_silver.sql

---
+++ Tests/quality_checks_silver.sql
@@ -0,0 +1,44 @@
+/*
+===============================================================================
+Quality Checks for Silver Layer
+===============================================================================
+
+Purpose:
+    This script validates data quality in the 'silver' layer by checking for:
+    - Null or duplicate primary keys
+    - Unwanted leading/trailing spaces in string fields
+    - Data standardization and formatting issues
+    - Invalid date ranges or chronological inconsistencies
+    - Consistency between related fields
+
+Usage:
+    - Execute after loading the Silver Layer.
+    - Review and resolve any anomalies detected.
+
+===============================================================================
+*/
+
+-- ====================================================================
+-- Quality Check: 'silver.crm_cust_info' Table
+-- ====================================================================
+
+-- 1. Check for NULL or Duplicate Primary Keys
+-- Expectation: Query should return zero rows (i.e., no issues)
+SELECT 
+    cst_id,
+    COUNT(*) 
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- 2. Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    cst_key 
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
+
+-- 3. Data Standardization & Consistency
+SELECT DISTINCT 
+    cst_marital_status 
+FROM silver.crm_cust_info;
