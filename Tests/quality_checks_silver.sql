bc6a69fd1050a8700b7dd1f172d2547b610eda80
Lasmar Safouane <146588910+safone21@users.noreply.github.com>
2025-04-29 20:29:55 +0100

    Update quality_checks_silver.sql

--- Tests/quality_checks_silver.sql
+++ Tests/quality_checks_silver.sql
@@ -42,3 +42,43 @@ WHERE cst_firstname != TRIM(cst_firstname);
 SELECT DISTINCT 
     cst_marital_status 
 FROM silver.crm_cust_info;
+
+
+-- ====================================================================
+-- Quality Check: 'silver.crm_prd_info' Table
+-- ====================================================================
+
+-- 1. Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+    prd_id,
+    COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- 2. Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- 3. Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- 4. Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
+
+-- 5. Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
