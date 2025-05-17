925b4ca02d1f4767246b492eeb877a5275470012
Lasmar Safouane <146588910+safone21@users.noreply.github.com>
2025-05-17 13:46:31 +0100

    Update quality_checks_gold.sql

--- Tests/quality_checks_gold.sql
+++ Tests/quality_checks_gold.sql
@@ -39,3 +39,17 @@ SELECT
 FROM gold.dim_products
 GROUP BY product_key
 HAVING COUNT(*) > 1;
+
+
+
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+--1. Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
